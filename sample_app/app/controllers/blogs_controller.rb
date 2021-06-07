aa834625d6bd486f41b1a3cb1b379cfb7bf91ad2
sykg1204yuki <y.yuki040507@gmail.com>
2021-06-07 01:58:22 +0000

    sample_app作成

--- sample_app/app/controllers/blogs_controller.rb
+++ sample_app/app/controllers/blogs_controller.rb
@@ -1,13 +1,35 @@
 class BlogsController < ApplicationController
   def index
+    @blogs = Blog.all
   end
 
   def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
+    @blogs = Blog.new
+  end
+  
+  def create
+    blog = Blog.new(blog_params)
+    blog.save
+    redirect_to blog_path(blog.id)
   end
 
   def edit
+    @blog = Blog.find(params[:id])
   end
+  
+  def update
+    blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog.id)
+  end 
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :category, :body)
+  end 
+  
 end
